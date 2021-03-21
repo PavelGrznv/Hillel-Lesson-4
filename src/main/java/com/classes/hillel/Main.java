7d4e4c21a3cf193e2cf7b6ec07f69e74b88e31c3
Ali <jminhyokwon@gmail.com>
2021-03-21 13:37:32 +0200

    add new message

--- src/main/java/com/classes/hillel/Main.java
+++ src/main/java/com/classes/hillel/Main.java
@@ -3,5 +3,6 @@ package main.java.com.classes.hillel;
 public class Main {
     public static void main (String [] args){
         System.out.println("Classno");
+        System.out.println("HochuDomoi");
     }
 }
