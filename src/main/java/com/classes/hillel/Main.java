78888e4fd9a58c3b5e16ad09baeded91f06504a0
Ali <jminhyokwon@gmail.com>
2021-03-21 14:56:58 +0200

    created arrays; wrote array to the console;

--- src/main/java/com/classes/hillel/Main.java
+++ src/main/java/com/classes/hillel/Main.java
@@ -2,7 +2,17 @@ package main.java.com.classes.hillel;
 
 public class Main {
     public static void main (String [] args){
-        System.out.println("Classno");
-        System.out.println("HochuDomoi");
+        String[] array = new String [2];
+        int[] array2 = {1,2,3};
+        array[0] = "Hello";
+        array[1] = "world";
+        System.out.println(array[array.length-1]);
+
+       for (int i = 0; i<array.length; i++) {
+           System.out.println(array[i]);
+       }
+       for (int i = 0; i<array2.length; i++) {
+           System.out.println(array2[i]);
+       }
     }
 }
