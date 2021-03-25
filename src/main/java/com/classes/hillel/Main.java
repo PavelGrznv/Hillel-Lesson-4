1b6f4ea7e7173707b506fac859c8a3bbd0b19f88
Pavel <pashagruznov@gmail.com>
2021-03-25 12:42:42 +0200

    delete something useless and added some new feature

--- src/main/java/com/classes/hillel/Main.java
+++ src/main/java/com/classes/hillel/Main.java
@@ -1,8 +1,63 @@
 package main.java.com.classes.hillel;
 
 public class Main {
-    public static void main (String [] args){
-        System.out.println("Classno");
-        System.out.println("HochuDomoi");
+    public static void main(String[] args) {
+
+        String[] array = new String[2];
+        int[] array2 = {1, 2, 3};
+
+//       array[0] = "Hello";
+//       array[1] = "world";
+//
+//       System.out.println(array[array.length - 1]);
+
+        String[] alphabet = {"a", "b", "c", "d",
+                "e", "f", "g", "h", "i", "j", "k",
+                "l", "m", "n", "o", "p", "q", "r",
+                "s", "t", "u", "v", "w", "x", "y", "z",
+                " ", "!", "?",".",",",":",";","+","-","=","/","*","_",")","("};
+
+        String encryptedWord = encrypt("abc abc !",
+                alphabet, 3);
+        System.out.println(encryptedWord);
+        String decryptedWord = decrypt("def.def.,",alphabet,-3);
+        System.out.println(decryptedWord);
+    }
+
+
+    private static String encrypt(String word, String[] alphabet, int shift) {
+        String encrypted = "";
+        for (int i = 0; i < word.length(); i++) {
+            int index = findIndex(alphabet, word.charAt(i));
+            if (index + shift > alphabet.length - 1) {
+                int innerIndex = (index + shift) - alphabet.length;
+                index = innerIndex - 1;
+            }
+            encrypted = encrypted.concat(alphabet[index + shift]);
+        }
+        return encrypted;
+    }
+    private static int findIndex(String[] alphabet, char letter) {
+        int index = 0;
+        for (int i = 0; i < alphabet.length; i++) {
+            String ch = alphabet[i];
+            if (ch.equals(String.valueOf(letter))) {
+                return i;
+            }
+        }
+        return index;
+
+    }
+    private static  String decrypt(String word, String[] alphabet, int shift){
+        String decrypted = "";
+        for (int i = 0; i<word.length(); i++) {
+            int index = findIndex(alphabet, word.charAt(i));
+            if (index + shift > alphabet.length - 1){
+                int innerIndex = (index + shift) - alphabet.length;
+                index = innerIndex - 1;
+            }
+            decrypted = decrypted.concat(alphabet[index + shift]);
+        }
+        return decrypted;
     }
 }
